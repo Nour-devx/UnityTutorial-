38803eb17c3bc1187bdcdf981b2ac98d2e0bca43
Nour Mohamed <nour@Nours-MBP.hsd1.ca.comcast.net>
2021-01-22 20:51:14 -0800

    create person scripts that takes name, age and weight then returns a text representing the person

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public InputField nameInput;
+    public InputField ageInput;
+    public InputField weightInput;
+    public Text result;
+
+    // used in the inspector 
+    public void CreatePerson()
+    { 
+        Person newPerson = new Person(nameInput.text, float.Parse(weightInput.text), true, int.Parse(ageInput.text), 180);
+        result.text += newPerson.firstName + " weight: " + newPerson.weightInKg + " age: " + newPerson.age;
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,51 @@
+﻿
+
+public class Person 
+{
+    // instance variables
+    // functions or methods
+    // object
+    // encapslation type Name {body}
+    // oop -> inheritance 
+    // instance variables
+
+
+  
+
+    public string CompareWeight(Person hamada)
+    {
+        if (weightInKg> hamada.weightInKg)
+        {
+            return firstName+" is heavier";
+        }
+
+        else
+        {
+            return hamada.firstName+" is heavier";
+        }
+    }
+
+    public string firstName;
+
+    public bool femaleOrMale;
+
+    public float heightInCms;
+
+    public int age = 20;
+
+    public float weightInKg;
+
+
+    public Person(string firstName,float weightInKg, bool femaleOrMale, int age, float heightInCms)
+    {
+        this.firstName = firstName;
+        this.weightInKg = weightInKg;
+        this.femaleOrMale = femaleOrMale;
+        this.age = age;
+        this.heightInCms = heightInCms;
+    }
+
+}
+
+
+
